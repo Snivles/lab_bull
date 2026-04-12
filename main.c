2e0cd574271309a233e6f91d13f004b7b32775b6
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-04-12 22:21:01 +0700

    реализация сумма по модулю 2

--- main.c
+++ main.c
@@ -35,6 +35,26 @@ unsigned char *log_ymnojenie(unsigned char *vecA, int lenA,unsigned char *vecB, 
     return result;}
   return NULL;}
 
+
+//Искл или(Сумма по модулю 2)
+// 101001110 - 1 вектор
+// 010110001 - 2 вектор
+// 111111111 - ответ(т е если в 1 векторе бит равен 1 , а в другом 0, то ставим 1)
+unsigned char *sumMod2(unsigned char *vecA, int lenA,unsigned char *vecB, int lenB) {
+    if((lenA==lenB) && vecA && vecB ){
+    unsigned char *result = NULL;
+    size_t size_byte = ((lenA +7) / 8);
+    result = (unsigned char*)malloc(size_byte);
+
+    if (result) {
+        for (size_t i = 0; i < size_byte; i++) {
+            result[i] = vecA[i] ^ vecB[i];}}
+    return result;}
+  return NULL;}
+
+
+
+
 int main()
 {
     printf("Hello World!\n");

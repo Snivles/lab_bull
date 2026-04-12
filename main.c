e64f54f59677974765b0c90faffc16337ee4e654
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-04-12 22:13:33 +0700

    добавление функции логического умножения

--- main.c
+++ main.c
@@ -8,12 +8,30 @@
 unsigned char *log_sum(unsigned char *vecA, int lenA,unsigned char *vecB, int lenB) {
     if((lenA==lenB) && vecA && vecB ){
     unsigned char *result = NULL;
-    size_t size_byte = ((lenA - 1) / 8 + 1);
+    size_t size_byte = ((lenA +7) / 8);
     result = (unsigned char*)malloc(size_byte);
 
     if (result) {
         for (size_t i = 0; i < size_byte; i++) {
             result[i] = vecA[i] | vecB[i];}}
+    return result;}
+  return NULL;}
+
+
+//логическое умножение
+// 101001110 - 1 вектор
+// 010110001 - 2 вектор
+// 000000000 - ответ(т е если в 1 векторе бит равен 1 , а в другом 1, то ставим 1 в других случаях 0) !! обратное искл или
+
+unsigned char *log_ymnojenie(unsigned char *vecA, int lenA,unsigned char *vecB, int lenB) {
+    if((lenA==lenB) && vecA && vecB ){
+    unsigned char *result = NULL;
+    size_t size_byte = ((lenA +7) / 8);
+    result = (unsigned char*)malloc(size_byte);
+
+    if (result) {
+        for (size_t i = 0; i < size_byte; i++) {
+            result[i] = vecA[i] & vecB[i];}}
     return result;}
   return NULL;}
 
